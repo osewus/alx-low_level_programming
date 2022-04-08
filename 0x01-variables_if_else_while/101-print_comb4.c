bf59e31dffb49ec34f744328df30d46bf99509bd
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-04-08 16:53:43 -0700

    Task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, k;
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+putchar((k % 10) + '0');
+
+if (i == 7 && j == 8 && k == 9)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
