0a1cc912260bc404c2512dbbd7c493de5584fc3a
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-04-08 10:32:51 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q' || alphabet == 'e')
+		continue;
+
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+return (0);
