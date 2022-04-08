69f5f156370a286c37b36ceae16aaa9650d6c1bd
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-04-08 11:17:43 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d\n", digit);
+	}
+
+	return (0);
+}
