4e93f8a6cb11e975d0ee26e298df222c4213fa1c
osewus <34625293+osewus@users.noreply.github.com>
2022-04-08 22:57:01 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,17 +13,17 @@ int main(void)
 {
 	int i; 
 	int j;
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			if (i != j && i < j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(',');
-				putchar(' ');
-			}
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			
+			if (i == 8 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
