21685e7f8f75d8288cc01e1c9bae5eb9455c7024
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-04-08 11:34:39 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+	{
+		putchar(s);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
