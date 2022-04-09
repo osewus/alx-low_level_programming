e786287471cec9414e8fb0077f210f06296772dc
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-04-08 17:23:25 -0700

    Task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
+
 #include <time.h>
-/* more headers goes there */
+
+/* more headers goes there */ 
 #include <stdio.h>
 
 /* betty style doc for function main goes there */
@@ -9,30 +11,20 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-int a, b;
+int main(void) {
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = i + 1; j < 10; j++) {
+      putchar((i % 10) + '0');
+      putchar((j % 10) + '0');
 
-for (a = 0; a <= 98; a++);
-{
-for (b = a + 1; b <= 99; b++);
-{
-putchar((a / 10) + '0');
-putchar((a % 10) + '0');
-putchar(' ');
-putchar((b / 10) + '0');
-putchar((b % 10) + '0');
+      if (i == 8 && j == 9)
+        continue;
 
-if (a == 98 && b == 99)
-continue;
-
-putchar(',');
-putchar(' ');
-}
-}
-
-putchar('\n');
-
-return (0);
-
+      putchar(',');
+      putchar(' ');
+    }
+  }
+  putchar('\n');
+  return (0);
 }
