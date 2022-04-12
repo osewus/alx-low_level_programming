aebea21247a08a795ee42836ac3138fe395d472a
osewus <34625293+osewus@users.noreply.github.com>
2022-04-12 14:26:37 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
-
 /*more headers goes there*/ 
 #include <stdio.h>
 #include "main.h"
-
 /*betty style doc for function main goes there */
 /**
  *print_alphabet_x10 - prints alphabets in lower case 10 times
