a39ffa7cd21a75ae5c3ad18c303d61ce5e120936
osewus <34625293+osewus@users.noreply.github.com>
2022-04-12 14:19:17 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,16 @@
 /* betty style doc for function main goes there */
 /**
  * print_alphabet_x10 - prints alphabets in lower case 10 times
- *
  **/
 void print_alphabet_x10(void) 
 {
-int count = 0;
-char letter;
-while (count++ <= 9) 
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-_putchar('\n');
-}
+  int count = 0;
+  char letter;
+  while (count++ <= 9) 
+  {
+    for (letter = 'a'; letter <= 'z'; letter++)
+      _putchar(letter);
+    _putchar('\n');
+  }
 }
 
