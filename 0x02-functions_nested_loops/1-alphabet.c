63a7b9e8ce79636a8dcdf0ce8360ab986a59c02e
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-04-12 06:16:44 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+
+		_Putchar('\n');
+	}
+}
