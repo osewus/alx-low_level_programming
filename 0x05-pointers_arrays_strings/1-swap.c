96ceb44d54248009b63bed9174e821873cf70185
osewus <34625293+osewus@users.noreply.github.com>
2022-04-20 18:35:46 +0000

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
 /**
  * 1-swap - swap values of two integers a and b
  * @a: A pointer to an int that will be updated
- * @b: A pointer to an int that will be updated
+ * @b: B pointer to an int that will be updated
  * Return: void that means our answer is correct
  */
 
