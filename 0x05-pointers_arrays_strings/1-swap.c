a0e81d89e2aaa250e71dc695f6e70c16e586cb3e
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-04-20 11:29:10 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * reset_to_98 - update an integer value to 98
+ * @n: A pointer to an int that will be updated
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+int i;
+i = *a;
+*a = *b;
+*b = i;
+}
+
+
