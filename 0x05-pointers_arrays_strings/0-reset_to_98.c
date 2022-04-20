09943bd3c976e34ffbf333f90c4deb909debba29
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-04-20 11:11:08 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - update an integer value to 98
+ * @n: A pointer to an int that will be updated
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
