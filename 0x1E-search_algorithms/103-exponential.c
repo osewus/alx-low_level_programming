03dacfa2219c69ca12e892d409b457b6012e05cf
osewus <34625293+osewus@users.noreply.github.com>
2022-11-02 02:41:40 +0000

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,70 +1,93 @@
 #include "search_algos.h"
 
-/**
- * _binary_search - Searches for a value in a sorted array
- *                  of integers using binary search.
- * @array: A pointer to the first element of the array to search.
- * @left: The starting index of the [sub]array to search.
- * @right: The ending index of the [sub]array to search.
- * @value: The value to search for.
- *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the index where the value is located.
- *
- * Description: Prints the [sub]array being searched after each change.
- */
-int _binary_search(int *array, size_t left, size_t right, int value)
-{
-	size_t i;
-
-	if (array == NULL)
-		return (-1);
-
-	while (right >= left)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
-		else
-			left = i + 1;
-	}
-
-	return (-1);
-}
+int binary_search(int *array, size_t size, int value);
+int recurse_helper(int *array, size_t left, size_t right, int value);
 
 /**
- * exponential_search - Searches for a value in a sorted array
- *                      of integers using exponential search.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- * @value: The value to search for.
+ * exponential_search - search array
+ * @array: array to search
+ * @size: size of array
+ * @value: search value
  *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the index where the value is located.
- *
- * Description: Prints a value every time it is compared in the array.
+ * Return: index of matched value; -1 if not found
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t i = 0, right;
+	size_t right = 1, left;
 
 	if (array == NULL)
 		return (-1);
 
-	if (array[0] != value)
+	while (right < size && array[right] < value)
 	{
-		for (i = 1; i < size && array[i] <= value; i = i * 2)
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", right, array[right]);
+		right *= 2;
 	}
 
-	right = i < size ? i : size - 1;
-	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
-	return (_binary_search(array, i / 2, right, value));
-}i
+	if (array[right] == value)
+		return (right);
+
+	left = right / 2; /* establish left bound */
+
+	if (right >= size) /* if right is out of bounds */
+		right = size - 1;
+
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+
+	return (recurse_helper(array, left, right, value));
+}
+
+/**
+ * binary_search - search for value in array of sorted ints
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search
+ *
+ * Return: index of found value; or -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+
+	return (recurse_helper(array, 0, size - 1, value));
+}
+
+/**
+ * recurse_helper - recursive implement of binary search
+ * @array: array to search
+ * @left: leftmost index
+ * @right: rightmost index
+ * @value: value to search
+ *
+ * Return: index of found value; or -1 if not found
+ */
+int recurse_helper(int *array, size_t left, size_t right, int value)
+{
+	size_t i = left, mid;
+
+	if (left > right)
+		return (-1);
+
+	/* print search progress */
+	printf("Searching in array: %d", array[i++]);
+	while (i <= right)
+		printf(", %d", array[i++]);
+	printf("\n");
+
+	/* calculate mid */
+	mid = left + ((right - left) / 2);
+
+	/* check if mid is value */
+	if (array[mid] == value)
+		return (mid);
+	else if (array[mid] > value)
+	{
+		if (mid != 0)
+			return (recurse_helper(array, left, mid - 1, value));
+		else
+			return (-1);
+	}
+	else
+		return (recurse_helper(array, mid + 1, right, value));
+}
